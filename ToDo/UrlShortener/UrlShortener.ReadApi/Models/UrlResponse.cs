567de26faab748570398bc5456f6f07d4937b61f
max arshinov <max.arshinov@gmail.com>
2025-06-16 18:24:26 +0200

    deleted todos

--- ToDo/UrlShortener/UrlShortener.ReadApi/Models/UrlResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace UrlShortener.ReadApi.Models;
-
-public class UrlResponse
-{
-    public string ShortUrl { get; set; } = string.Empty;
-    public string LongUrl { get; set; } = string.Empty;
-    public bool Success { get; set; }
-    public string? Message { get; set; }
-}
--- ToDo/UrlShortener/UrlShortener.ReadApi/Services/IUrlCache.cs
+++
@@ -1,8 +0,0 @@
-namespace UrlShortener.ReadApi.Services;
-
-public interface IUrlCache
-{
-    string? Get(string shortUrl);
-
-    void Set(string shortUrl, string longUrl);
-}
